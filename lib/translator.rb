f9a62f56533af4bceb0fdb64a261e89878234cd7
nick repasy <nrepasy@gmail.com>
2020-12-04 05:08:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,16 @@
-# require modules here
 
-def load_library
-  # code goes here
+require "yaml"
+require "pry"
+def load_library(file)
+  
+ hash = YAML.load(File.read("lib/emoticons.yml"))
+hash.each do |key, value|
+  key = {"emoticons" => {}}
+end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(text)
+
 end
 
 def get_english_meaning
